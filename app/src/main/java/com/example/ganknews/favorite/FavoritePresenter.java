34f2622e2898df7ed440cecce451e8431ec4cf6c
zhooker <zhuang42161@163.com>
2017-01-24 18:49:02 +0800

    数据库读取改成Rxjava形式

--- app/src/main/java/com/example/ganknews/favorite/FavoritePresenter.java
+++ app/src/main/java/com/example/ganknews/favorite/FavoritePresenter.java
@@ -10,6 +10,12 @@ import com.example.ganknews.favorite.presenter.FavoriteContacts;
 import com.example.ganknews.gank.model.GankInfo;
 
 import java.util.List;
+import java.util.Observable;
+
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by zhooker on 2016/10/11.
@@ -24,9 +30,36 @@ public class FavoritePresenter extends BasePresenter<FavoriteContacts.IFavoriteV
 
     @Override
     public void loadData() {
-        DaoSession daoSession = DaoManager.getInstance().getDaoSession();
-        GankInfoDao noteDao = daoSession.getGankInfoDao();
-        List<GankInfo> users = noteDao.loadAll();
-        mView.refreshList(users);
+        rx.Observable.create(new rx.Observable.OnSubscribe<List<GankInfo>>() {
+
+            @Override
+            public void call(Subscriber<? super List<GankInfo>> subscriber) {
+                DaoSession daoSession = DaoManager.getInstance().getDaoSession();
+                GankInfoDao noteDao = daoSession.getGankInfoDao();
+                try {
+                    List<GankInfo> users = noteDao.queryBuilder().orderDesc(GankInfoDao.Properties.Id).list();//.loadAll();
+                    subscriber.onNext(users);
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                } finally {
+                    subscriber.onCompleted();
+                }
+            }
+        }).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(new Subscriber<List<GankInfo>>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(List<GankInfo> gankInfos) {
+                mView.refreshList(gankInfos);
+            }
+        });
     }
 }
